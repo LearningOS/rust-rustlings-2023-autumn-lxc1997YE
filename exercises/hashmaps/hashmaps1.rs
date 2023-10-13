b1435820f9d6af3679d3d7dbf3e9ce24565142bf
lxc1997ye <leixiaochen97@163.com>
2023-10-13 22:04:35 +0800

    update score

--- exercises/hashmaps/hashmaps1.rs
+++ exercises/hashmaps/hashmaps1.rs
@@ -11,17 +11,31 @@
 // Execute `rustlings hint hashmaps1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new(); // TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
 
     // TODO: Put more fruits in your basket here.
+    basket.insert(String::from("apple"), 1);
+    basket.insert(String::from("mango"), 1);
+    basket.insert(String::from("pear"), 1);
+    basket.insert(String::from("orange"), 1);
+    basket.insert(String::from("lemon"), 1);
+    basket.insert(String::from("grape"), 1);
+    basket.insert(String::from("strawberry"), 1);
+    basket.insert(String::from("raspberry"), 1);
+    basket.insert(String::from("blueberry"), 1);
+    basket.insert(String::from("blackberry"), 1);
+    basket.insert(String::from("pineapple"), 1);
+    basket.insert(String::from("peach"), 1);
+    basket.insert(String::from("cherry"), 1);
+    basket.insert(String::from("papaya"), 1);
+    basket.insert(String::from("watermelon"), 1);
+    basket.insert(String::from("cantaloupe"), 1);
 
     basket
 }
